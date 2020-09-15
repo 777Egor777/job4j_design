3355c7cd9654795868cf9dea6f9c70dcb0029a7b
Egor <yegeraskin13@gmail.com>
2020-09-15 14:13:33 +0400

    5.1.1. Итератор для двухмерного массива int[][] [#292590]

---
+++ chapter_001/src/test/java/ru/job4j/it/MatrixItTest.java
@@ -0,0 +1,89 @@
+package ru.job4j.it;
+
+import org.junit.Test;
+
+import java.util.NoSuchElementException;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+
+public class MatrixItTest {
+
+    @Test
+    public void when1El() {
+        int[][] in = {
+                {1}
+        };
+        MatrixIt it = new MatrixIt(in);
+        assertThat(it.next(), is(1));
+    }
+
+    @Test
+    public void whenFirstEmptyThenNext() {
+        int[][] in = {
+                {}, {1}
+        };
+        MatrixIt it = new MatrixIt(in);
+        assertThat(it.next(), is(1));
+    }
+
+    @Test
+    public void whenFirstEmptyThenNasNext() {
+        int[][] in = {
+                {}, {1}
+        };
+        MatrixIt it = new MatrixIt(in);
+        assertThat(it.hasNext(), is(true));
+    }
+
+    @Test
+    public void whenRowHasDiffSize() {
+        int[][] in = {
+                {1}, {2, 3}
+        };
+        MatrixIt it = new MatrixIt(in);
+        assertThat(it.next(), is(1));
+        assertThat(it.next(), is(2));
+        assertThat(it.next(), is(3));
+    }
+
+    @Test
+    public void whenFewEmpty() {
+        int[][] in = {
+                {1}, {}, {}, {}, {2}
+        };
+        MatrixIt it = new MatrixIt(in);
+        assertThat(it.next(), is(1));
+        assertThat(it.next(), is(2));
+    }
+
+    @Test
+    public void whenEmptyCallHasNext() {
+        int[][] in = {
+                {}
+        };
+        MatrixIt it = new MatrixIt(in);
+        assertFalse(it.hasNext());
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void whenEmptyCallNext() {
+        int[][] in = {
+                {}
+        };
+        MatrixIt it = new MatrixIt(in);
+        it.next();
+    }
+
+    @Test
+    public void whenMultiHasNext() {
+        int[][] in = {
+                {}, {1}
+        };
+        MatrixIt it = new MatrixIt(in);
+        assertTrue(it.hasNext());
+        assertTrue(it.hasNext());
+        it.next();
+        assertFalse(it.hasNext());
+    }
+}
