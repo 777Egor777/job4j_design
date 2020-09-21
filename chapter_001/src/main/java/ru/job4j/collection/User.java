87077559b209142305c4b5222c955130ba1245fb
Egor <yegeraskin13@gmail.com>
2020-09-21 14:26:19 +0400

    2. Не перекрывать equals hashCode [#292607]

--- chapter_001/src/main/java/ru/job4j/collection/User.java
+++ chapter_001/src/main/java/ru/job4j/collection/User.java
@@ -1,6 +1,9 @@
 package ru.job4j.collection;
 
 import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.HashMap;
+import java.util.Map;
 
 public class User {
     private String name;
@@ -23,5 +26,15 @@ public class User {
 
     public Calendar getBirthday() {
         return birthday;
+    }
+
+    public static void main(String[] args) {
+        Calendar calendar = new GregorianCalendar(1995, 6, 27);
+        User firstUser = new User("Egor", 0, calendar);
+        User secondUser = new User("Egor", 0, calendar);
+        Map<User, Object> map = new HashMap<>();
+        map.put(firstUser, new Object());
+        map.put(secondUser, new Object());
+        System.out.println(map);
     }
 }
