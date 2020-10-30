f7a3bd45197ab9f39bcda5743adde3533a57bb2a
Egor <yegeraskin13@gmail.com>
2020-10-30 11:49:03 +0400

    1. Читаем файл конфигурации [#292631]

---
+++ chapter_002/src/main/java/ru/job4j/io/Config.java
@@ -0,0 +1,54 @@
+package ru.job4j.io;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.*;
+
+public class Config {
+    private final String path;
+    private final Map<String, String> values = new HashMap<>();
+
+    public Config(final String path) {
+        this.path = path;
+    }
+
+    public void load() {
+        List<String> lines = new ArrayList<>();
+        try (BufferedReader reader = new BufferedReader(new FileReader(path))) {
+            reader.lines().forEach(lines::add);
+            for (String line : lines) {
+                if (!line.equals("") && !line.startsWith("##")) {
+                    String[] parts = line.split("=");
+                    values.put(parts[0], parts[1]);
+                }
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public String value(String key) {
+        String result = "";
+        if (!values.containsKey(key)) {
+            throw new NoSuchElementException("No value with such key");
+        } else {
+            result = values.get(key);
+        }
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        StringJoiner joiner = new StringJoiner(System.lineSeparator());
+        try (BufferedReader reader = new BufferedReader(new FileReader(path))) {
+            reader.lines().forEach(joiner::add);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return joiner.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Config("app.properties"));
+    }
+}
---
+++ chapter_002/src/test/java/ru/job4j/io/ConfigTest.java
@@ -0,0 +1,25 @@
+package ru.job4j.io;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class ConfigTest {
+
+    @Test
+    public void whenPairWithoutComment() {
+        String path = "./data/pair_without_comment.properties";
+        Config config = new Config(path);
+        config.load();
+        assertThat(config.value("name"), is("Egor Geraskin"));
+    }
+
+    @Test
+    public void whenPairWithComment() {
+        String path = "./data/pair_with_comment.properties";
+        Config config = new Config(path);
+        config.load();
+        assertThat(config.value("city"), is("Saratov"));
+    }
+}
