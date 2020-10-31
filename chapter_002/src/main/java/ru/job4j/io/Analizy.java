746b3fa65e4cccead7798822e5f8685449a143f9
Egor <yegeraskin13@gmail.com>
2020-10-31 15:20:57 +0400

    2. Анализ доступности сервера. [#292632](fix #1)

--- chapter_002/src/main/java/ru/job4j/io/Analizy.java
+++ chapter_002/src/main/java/ru/job4j/io/Analizy.java
@@ -6,13 +6,11 @@ import java.util.List;
 
 public class Analizy {
     public void unavailable(String source, String target) {
-        List<String> periods = new LinkedList<>();
         try (BufferedReader reader = new BufferedReader(new FileReader(source));
              PrintWriter writer = new PrintWriter(new BufferedOutputStream(new FileOutputStream(target)))) {
-            List<String> lines = new LinkedList<>();
-            reader.lines().forEach(lines::add);
             String startOfPeriod = null;
-            for (String line : lines) {
+            String line;
+            while ((line = reader.readLine()) != null) {
                 String[] parts = line.split(" ");
                 int type = Integer.parseInt(parts[0]);
                 String date = parts[1];
@@ -22,16 +20,13 @@ public class Analizy {
                     }
                 } else {
                     if (startOfPeriod != null) {
-                        periods.add(startOfPeriod + ";" + date);
+                        writer.println(startOfPeriod + ";" + date);
                         startOfPeriod = null;
                     }
                 }
             }
             if (startOfPeriod != null) {
-                periods.add(startOfPeriod + ";");
-            }
-            for (String period : periods) {
-                writer.println(period);
+                writer.println(startOfPeriod + ";");
             }
         } catch (Exception ex) {
             ex.printStackTrace();
