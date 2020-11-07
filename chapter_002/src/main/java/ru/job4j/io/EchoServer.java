232d6f9f61ec5d7e1a201075bc41d40200aa2d67
Egor <yegeraskin13@gmail.com>
2020-11-07 18:21:45 +0400

    1. Бот [#292642]

--- chapter_002/src/main/java/ru/job4j/io/EchoServer.java
+++ chapter_002/src/main/java/ru/job4j/io/EchoServer.java
@@ -8,6 +8,12 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class EchoServer {
+    public static String getMsg(String line) {
+        String result = line.split("msg=")[1];
+        result = result.split(" ")[0];
+        return result;
+    }
+
     public static void main(String[] args) throws IOException {
         try (ServerSocket server = new ServerSocket(9000)) {
             boolean work = true;
@@ -18,15 +24,25 @@ public class EchoServer {
                              new InputStreamReader(socket.getInputStream()))) {
                     String line;
                     int indexOfLine = 0;
+                    String answer = null;
                     while (!(line = in.readLine()).isEmpty()) {
                         if (indexOfLine++ == 0
-                           && line.contains("?msg=Bye")) {
-                            work = false;
-                            break;
+                           && line.contains("?msg=")) {
+                            String msg = getMsg(line);
+                            if (msg.equals("Hello")) {
+                                answer = "Hello, my dear friend";
+                            } else if (msg.equals("Exit")) {
+                                work = false;
+                            } else {
+                                answer = msg;
+                            }
                         }
                         System.out.println(line);
                     }
                     out.write("HTTP/1.1 200 OK\r\n\\".getBytes());
+                    if (answer != null) {
+                        out.write((answer + "\r\n\\").getBytes());
+                    }
                 }
             }
         }
