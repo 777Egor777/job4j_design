04a4a9b3bb4cf6cdba6b4986a33a631102efe2a3
Egor <yegeraskin13@gmail.com>
2020-11-08 19:18:31 +0400

    4. Slf4j - вывод exception. [#292622]

--- chapter_002/src/main/java/ru/job4j/io/EchoServer.java
+++ chapter_002/src/main/java/ru/job4j/io/EchoServer.java
@@ -1,5 +1,8 @@
 package ru.job4j.io;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -8,13 +11,15 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class EchoServer {
+    private static final Logger LOG = LoggerFactory.getLogger(EchoServer.class.getName());
+
     public static String getMsg(String line) {
         String result = line.split("msg=")[1];
         result = result.split(" ")[0];
         return result;
     }
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         try (ServerSocket server = new ServerSocket(9000)) {
             boolean work = true;
             while (work) {
@@ -22,10 +27,10 @@ public class EchoServer {
                 try (OutputStream out = socket.getOutputStream();
                      BufferedReader in = new BufferedReader(
                              new InputStreamReader(socket.getInputStream()))) {
-                    String line;
+                    String line = in.readLine();
                     int indexOfLine = 0;
                     String answer = null;
-                    while (!(line = in.readLine()).isEmpty()) {
+                    while (!line.isEmpty()) {
                         if (indexOfLine++ == 0
                            && line.contains("?msg=")) {
                             String msg = getMsg(line);
@@ -38,13 +43,17 @@ public class EchoServer {
                             }
                         }
                         System.out.println(line);
+                        line = in.readLine();
                     }
                     out.write("HTTP/1.1 200 OK\r\n\\".getBytes());
                     if (answer != null) {
                         out.write((answer + "\r\n\\").getBytes());
                     }
+
                 }
             }
+        } catch (IOException e) {
+            LOG.error("IOException", e);
         }
     }
 }
